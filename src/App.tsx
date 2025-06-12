7875e1e18f92ecdb3e71bda8e551476c058ca35c
dobryakk5 <system5@mail.ru>
2025-06-12 20:30:52 +0300

    fix: small2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import Dashboard from './pages/Dashboard';
 import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 
-function App() {
+export default function App(){
   return (
     <WebAppProvider options={{ smoothButtonsTransition: true }}>
       <Dashboard />
